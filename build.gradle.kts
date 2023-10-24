fba6c3bbbee75d1d866aa02a0ccdb924b3b2575f
DatLag <jeff.retz08@gmail.com>
2023-10-24 22:01:39 +0200

    added info about output file

--- build.gradle.kts
+++ build.gradle.kts
@@ -95,6 +95,9 @@ tasks.withType<DependencyUpdatesTask> {
             }
         }
         val outputFile = layout.buildDirectory.file("dependencyUpdates/report.md").get().asFile
+        println("Report file: ${outputFile.name}")
+        println("Report file path: ${outputFile.path}")
+        println("Report file absolute path: ${outputFile.path}")
         try {
             if (outputFile.exists()) {
                 outputFile.delete()
